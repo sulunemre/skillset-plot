e791a907757afbce9bf9211694cf0878e155e1eb
Emre Sülün <emresulun93@gmail.com>
2018-11-20 18:22:09 +0300

    First release

--- script.js
+++ script.js
@@ -1,33 +1,91 @@
-var trace1 = {
-	x: [1, 2, 3, 4, 5],
-	y: [1, 6, 3, 6, 1],
+function indexOf(categories, categoryName){
+	for(let i = 0; i < categories.length; i++){
+		if(categories[i].name === categoryName){
+			return i;
+		}
+	}
+	return -1;
+}
+
+function readTextFile(file, callback) {
+	var rawFile = new XMLHttpRequest();
+	rawFile.overrideMimeType("application/json");
+	rawFile.open("GET", file, false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4 && rawFile.status == "200") {
+			callback(rawFile.responseText);
+		}
+	}
+	rawFile.send(null);
+}
+
+var categories = []; // "Traces" in the original example
+
+var knowScores = [];
+var loveScores = [];
+var names = [];
+
+// https://stackoverflow.com/a/34579496/5964489
+readTextFile("example-input.json", function (text) {
+	const data = JSON.parse(text);
+
+	// Traverse all skills
+	for (var i = 0; i < data.length; i++) {
+		let category = data[i]["category"];
+		let index = indexOf(categories, category);
+		if (index < 0) {
+			categories.push({
+				x: [data[i]["know-score"]],
+				y: [data[i]["love-score"]],
+				mode: 'markers',
+				type: 'scatter',
+				name: category,
+				text: [data[i]["name"]],
+				marker: {size: 12}
+			});
+
+		}
+		else {
+			categories[index].x.push(data[i]["know-score"]);
+			categories[index].y.push(data[i]["love-score"]);
+			categories[index].text.push(data[i]["name"]);
+		}
+	}
+});
+
+
+let trace1 = {
+	x: knowScores,
+	y: loveScores,
 	mode: 'markers',
 	type: 'scatter',
 	name: 'Team A',
-	text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-	marker: { size: 12 }
+	text: names,
+	marker: {size: 12}
 };
 
-var trace2 = {
-	x: [1.5, 2.5, 3.5, 4.5, 5.5],
-	y: [4, 1, 7, 1, 4],
+let trace2 = {
+	x: [15, 25, 35, 45, 55],
+	y: [40, 10, 70, 10, 40],
 	mode: 'markers',
 	type: 'scatter',
 	name: 'Team B',
 	text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
-	marker: { size: 12 }
+	marker: {size: 12}
 };
 
-var data = [ trace1, trace2 ];
+let data = categories;
 
-var layout = {
+let layout = {
 	xaxis: {
-		range: [ 0.75, 5.25 ]
+		range: [0, 100],
+		title: "Know"
 	},
 	yaxis: {
-		range: [0, 8]
+		range: [0, 100],
+		title: "Love"
 	},
-	title:'Data Labels Hover'
+	title: 'Development Skills'
 };
 
 Plotly.newPlot('myDiv', data, layout);
