6ade2d6aeedffbe61487f64907f193638b50f3e5
Yasin Balcancı <ybalcanci@gmail.com>
2018-11-20 18:16:15 +0300

    indexOf function for script.

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+var windowsHeight = window.screen.height;
+var windowsWidth = window.screen.width;
+
+var div = document.getElementsByClassName("plot-container plotly")[0];
+console.log(div);
+div.style.height = windowsHeight; 
+div.style.width = windowsWidth; 
+
 var country = ['Switzerland (2011)', 'Chile (2013)', 'Japan (2014)', 'United States (2012)', 'Slovenia (2014)', 'Canada (2011)', 'Poland (2010)', 'Estonia (2015)', 'Luxembourg (2013)', 'Portugal (2011)'];
 
 var votingPop = [40, 45.7, 52, 53.6, 54.1, 54.2, 54.5, 54.7, 55.1, 56.6];
 
 var regVoters = [49.1, 42, 52.7, 84.3, 51.7, 61.1, 55.3, 64.2, 91.1, 58.9];
+
+class Skill{
+	constructor(category, name, knowScore, loveScore){
+		this.category = category;
+		this.name = name;
+		this.knowScore = knowScore;
+		this.loveScore = loveScore;
+	}
+}
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
 
 var trace1 = {
   type: 'scatter',
@@ -37,10 +71,10 @@ var trace2 = {
   }
 };
 
-var data = [trace1, trace2];
+var data = [trace1];
 
 var layout = {
-  title: 'Votes cast for ten lowest voting age population in OECD countries',
+  title: 'Skillset Plot',
   xaxis: {
     showgrid: false,
     showline: true,
@@ -76,8 +110,8 @@ var layout = {
   width: 600,
   height: 400,
   paper_bgcolor: 'rgb(254, 247, 234)',
-  plot_bgcolor: 'rgb(254, 247, 234)',
+  plot_bgcolor: 'rgb(200, 247, 234)',
   hovermode: 'closest'
 };
-
+readTextFile("example-input.json");
 Plotly.newPlot('myDiv', data, layout);
