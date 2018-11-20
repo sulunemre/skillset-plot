1c6da059c32f95c348e91cd73b0df65647fad9cd
Emre Sülün <emresulun93@gmail.com>
2018-11-20 17:14:06 +0300

    Second UI initialization

--- script.js
+++ script.js
@@ -1,83 +1,33 @@
-var country = ['Switzerland (2011)', 'Chile (2013)', 'Japan (2014)', 'United States (2012)', 'Slovenia (2014)', 'Canada (2011)', 'Poland (2010)', 'Estonia (2015)', 'Luxembourg (2013)', 'Portugal (2011)'];
-
-var votingPop = [40, 45.7, 52, 53.6, 54.1, 54.2, 54.5, 54.7, 55.1, 56.6];
-
-var regVoters = [49.1, 42, 52.7, 84.3, 51.7, 61.1, 55.3, 64.2, 91.1, 58.9];
-
 var trace1 = {
-  type: 'scatter',
-  x: votingPop,
-  y: country,
-  mode: 'markers',
-  name: 'Percent of estimated voting age population',
-  marker: {
-    color: 'rgba(156, 165, 196, 0.95)',
-    line: {
-      color: 'rgba(156, 165, 196, 1.0)',
-      width: 1,
-    },
-    symbol: 'circle',
-    size: 16
-  }
+	x: [1, 2, 3, 4, 5],
+	y: [1, 6, 3, 6, 1],
+	mode: 'markers',
+	type: 'scatter',
+	name: 'Team A',
+	text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+	marker: { size: 12 }
 };
 
 var trace2 = {
-  x: regVoters,
-  y: country,
-  mode: 'markers',
-  name: 'Percent of estimated registered voters',
-  marker: {
-    color: 'rgba(204, 204, 204, 0.95)',
-    line: {
-      color: 'rgba(217, 217, 217, 1.0)',
-      width: 1,
-    },
-    symbol: 'circle',
-    size: 16
-  }
+	x: [1.5, 2.5, 3.5, 4.5, 5.5],
+	y: [4, 1, 7, 1, 4],
+	mode: 'markers',
+	type: 'scatter',
+	name: 'Team B',
+	text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
+	marker: { size: 12 }
 };
 
-var data = [trace1, trace2];
+var data = [ trace1, trace2 ];
 
 var layout = {
-  title: 'Votes cast for ten lowest voting age population in OECD countries',
-  xaxis: {
-    showgrid: false,
-    showline: true,
-    linecolor: 'rgb(102, 102, 102)',
-    titlefont: {
-      font: {
-        color: 'rgb(204, 204, 204)'
-      }
-    },
-    tickfont: {
-      font: {
-        color: 'rgb(102, 102, 102)'
-      }
-    },
-    autotick: false,
-    dtick: 10,
-    ticks: 'outside',
-    tickcolor: 'rgb(102, 102, 102)'
-  },
-  margin: {
-    l: 140,
-    r: 40,
-    b: 50,
-    t: 80
-  },
-  legend: {
-    font: {
-      size: 10,
-    },
-    yanchor: 'middle',
-    xanchor: 'right'
-  },
-  width: 600,
-  height: 400,
-  paper_bgcolor: 'rgb(254, 247, 234)',
-  plot_bgcolor: 'rgb(254, 247, 234)',
-  hovermode: 'closest'
+	xaxis: {
+		range: [ 0.75, 5.25 ]
+	},
+	yaxis: {
+		range: [0, 8]
+	},
+	title:'Data Labels Hover'
 };
 
 Plotly.newPlot('myDiv', data, layout);
